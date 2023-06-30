5766e7e8fe652b46b0e701a93de8ee720c6b0543
Josué Lustosa <josuelustosa.job@gmail.com>
2023-06-30 07:54:58 -0400

    refactor: arrow function em linha

--- api-breakingnews/index.js
+++ api-breakingnews/index.js
@@ -7,6 +7,6 @@ const userRouter = require("./src/routes/user.route");
 // ROTAS: método + nome da rota + função (callback)
 app.use("/soma", userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Aplicação Node + Express rodando na porta ${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Aplicação Node + Express rodando na porta ${PORT}`)
+);
